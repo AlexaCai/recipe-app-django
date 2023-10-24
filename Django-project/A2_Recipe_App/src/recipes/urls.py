23dca5015290814c1fa1c4b16c81c6707300e9f3
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-10-24 13:04:39 -0600

    Comment updates

--- Django-project/A2_Recipe_App/src/recipes/urls.py
+++ Django-project/A2_Recipe_App/src/recipes/urls.py
@@ -3,8 +3,8 @@ from .views import home
 
 app_name = "recipes"
 
-# I want to have a custom homepage instead of the default Django welcome page when \
-# 'http://127.0.0.1:8000/' is visited. Since nothing is written after the main \
+# Since we want to have a custom homepage instead of the default Django welcome page when \
+# 'http://127.0.0.1:8000/' is visited, and since nothing is written after the main \
 # 'http://127.0.0.1:8000/' URL, the route parameter is currently empty '', in (path('', ...)).
 # This specifies that when “http://127.0.0.1:8000/” is visited, you’ll see the page specified by \
 # home, rather than the default Django welcome page.
