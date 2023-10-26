1be58ddde836c3a71240555fed46328a543fae31
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-10-25 21:26:12 -0600

    Update to allow user-generated images

--- Django-project/A2_Recipe_App/src/recipe_project/urls.py
+++ Django-project/A2_Recipe_App/src/recipe_project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 # The empty quotes '' indicate that this path refers to the home page 'http://127.0.0.1:8000/' while \
@@ -26,3 +28,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
