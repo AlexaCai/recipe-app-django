0692bcac955c26fbf7a30c262aff874e356cd443
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-10-25 21:26:47 -0600

    Update for users to enter more precise ingredients

--- Django-project/A2_Recipe_App/src/recipes/admin.py
+++ Django-project/A2_Recipe_App/src/recipes/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, RecipeIngredients  # Import the RecipeIngredients model
 
-# Register your models here.
+# Define an inline formset for the RecipeIngredients model
+from django.forms import inlineformset_factory
 
-admin.site.register(Recipe)
+class RecipeIngredientsInline(admin.TabularInline):
+    model = RecipeIngredients
+    extra = 1
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cooking_time', 'origin_country', 'creation_date')
+    inlines = [RecipeIngredientsInline]
+
+admin.site.register(Recipe, RecipeAdmin)
