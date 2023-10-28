77b354ac7ff119b60ba96bcd9882590dca396d22
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-10-28 17:43:25 -0600

    New view added

--- Django-project/A2_Recipe_App/src/recipes/views.py
+++ Django-project/A2_Recipe_App/src/recipes/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render, redirect
-from .models import Recipe, RecipeIngredients
+from django.shortcuts import render
+from django.views.generic import ListView      #to display lists
+from .models import Recipe                     #to access Book model
 
 # Create your views here.
 
 # Takes the request coming from the web application and returns the template available at \
 # recipes/recipes_home.html as a response.
 def home(request):
-   return render(request, 'recipes/recipes_home.html')
+   return render(request, 'recipes/home.html')
+
+class RecipeListView(ListView):                             #class-based view
+   model = Recipe                                           #specify model
+   template_name = 'recipes/unsigned_users_recipes.html'    #specify template 
