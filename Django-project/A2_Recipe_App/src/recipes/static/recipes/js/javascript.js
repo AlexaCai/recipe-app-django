d8d8b1ea8e8f715105a003074445a3003a6fc441
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-10-30 11:54:32 -0600

    Main search function responsiveness update

--- Django-project/A2_Recipe_App/src/recipes/static/recipes/js/javascript.js
+++ Django-project/A2_Recipe_App/src/recipes/static/recipes/js/javascript.js
@@ -34,7 +34,58 @@ function displaySearchResults(data) {
     searchResultsContainer.innerHTML = '';
 
     if (recipes.length === 0) {
-        searchResultsContainer.innerHTML = 'No recipes found.';
+        var searchResultsRow = document.createElement('div');
+        searchResultsRow.className = 'row';
+
+        var leftColumn = document.createElement('div');
+        leftColumn.className = 'col-sm-1 col-md-2'; 
+
+        var middleColumn = document.createElement('div');
+        middleColumn.className = 'col-sm-10 col-md-8 text-center';
+
+            var mainSearchNotFound = document.createElement('div');
+            mainSearchNotFound.className = 'card-padding';
+
+            mainSearchNotFound.innerHTML = `
+            <br>
+            <br>
+            <h1 class="recipes-continent-title-section">Recipe(s) not found</h1> 
+            <br>
+            <br>
+            <div class="search-result-not-found">
+                <span>Make sure you have written the name of your recipe correctly without mistakes.</span>
+                <br>
+                <br>
+                <span>***</span>
+                <br>
+                <br>
+                <span>You can also search using the filters in the 'Advanced Search' option for potentially more similar results.</span>
+                <br>
+                <br>
+                <span>***</span>
+                <br>
+                <br>
+                <span>If you still can't find your recipe, it's a sign! <a href="placeholder">Submit your recipe to us</a> so that we can display it for all users to enjoy. It's simple and it takes 2 minutes.</span>
+                <br>
+                <br>
+                <span>***</span>
+                <br>
+                <br>
+                <button type="button" class="btn btn-danger" onclick="clearSearch()">Clear search</button>
+            </div>
+            `;
+
+        var rightColumn = document.createElement('div');
+        rightColumn.className = 'col-sm-1 col-md-2'; 
+
+        middleColumn.appendChild(mainSearchNotFound);
+
+        searchResultsRow.appendChild(leftColumn);
+        searchResultsRow.appendChild(middleColumn);
+        searchResultsRow.appendChild(rightColumn);
+
+        searchResultsContainer.appendChild(searchResultsRow);
+
     } else {
         recipes.forEach(function (recipe) {
             var recipeCard = document.createElement('div');
@@ -64,15 +115,15 @@ function displaySearchResults(data) {
                         <div class="card-text">
                             <h5 class="card-title">${recipe.recipe_name}</h5>
                             <div class="line-background">
-                                <img src="../../../media/flag-icon.png" alt="flag-icon" style="width: 20px; height: 20px; margin-right: 5px;"><span>${ recipe.recipe_origin_country }</span>
+                                <img src="../../../media/flag-icon.png" alt="flag-icon" style="width: 20px; height: 20px; margin-right: 5px;"><span>${recipe.recipe_origin_country}</span>
                             </div>
                             <br>
                             <div class="line-background">
-                                <img src="../../../media/difficulty-icon.png" alt="flag-icon" style="width: 20px; height: 20px; margin-right: 5px;"><span>${ recipe.recipe_difficulty }</span>
+                                <img src="../../../media/difficulty-icon.png" alt="flag-icon" style="width: 20px; height: 20px; margin-right: 5px;"><span>${recipe.recipe_difficulty}</span>
                             </div>
                             <br>
                             <div class="line-background">
-                                <img src="../../../media/category-icon.png" alt="flag-icon" style="width: 20px; height: 20px; margin-right: 5px;"><span>${ recipe.recipe_category }</span>
+                                <img src="../../../media/category-icon.png" alt="flag-icon" style="width: 20px; height: 20px; margin-right: 5px;"><span>${recipe.recipe_category}</span>
                             </div>                        
                         </div>
                     </div>
@@ -93,6 +144,13 @@ function showSearchResultsSection() {
 
     var searchResultsSection = document.getElementById('search-results');
     searchResultsSection.style.display = 'block';
+}
+
+function clearSearch() {
+    var searchInput = document.getElementById('recipe-search-input');
+    searchInput.value = '';
+
+    window.location.href = '/recipes-list-unsigned-users'; 
 }
 
 //***Logic to handle the search by ingredients functionality
