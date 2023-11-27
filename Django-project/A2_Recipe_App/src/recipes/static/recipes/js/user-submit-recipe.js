bf81885d1af08d5b9fbd3307a114b11326149a4f
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-11-26 23:34:26 -0500

    Update on the recipe submit form

--- Django-project/A2_Recipe_App/src/recipes/static/recipes/js/user-submit-recipe.js
+++ Django-project/A2_Recipe_App/src/recipes/static/recipes/js/user-submit-recipe.js
@@ -1,34 +1,57 @@
 // Logic used to add dynamically more forms/lines to the ingredients formset on frontend, to allow users
 // to add as much ingredients to their recipe as they want when SUBMITTING recipes.
-
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOM fully loaded and parsed');
     var formsetContainer = document.getElementById('ingredients-formset-container');
     var addFormButton = document.getElementById('add-form');
-    console.log(addFormButton);  // This should log the button element
+    var removeFormButton = document.getElementById('remove-ingredient-form');
 
-    // Add form when button is clicked
+    // Initially, hide the "Remove Ingredient" button if there's only one formset
+    updateRemoveButtonVisibility();
+
+    // Add form when "add-form" button is clicked
     addFormButton.addEventListener('click', function () {
-        console.log('Button Clicked');
         var formsets = formsetContainer.getElementsByClassName('formset');
         var lastFormset = formsets[formsets.length - 1];
         var newFormset = lastFormset.cloneNode(true);
 
-        // Update the formsetContainer ID to avoid duplicate IDs
-        var formsetContainerCount = formsets.length;
-        updateFormInputNames(newFormset, formsetContainerCount);
+        // Clear the input fields in the new formset
+        var inputs = newFormset.querySelectorAll('input[type="text"], input[type="number"]');
+        for (var i = 0; i < inputs.length; i++) {
+            inputs[i].value = '';
+        }
+
+        // Clear the select fields in the new formset
+        var textarea = newFormset.querySelector('textarea');
+            textarea.value = '';
 
         // Append the new formset container after the last formset container
         formsetContainer.appendChild(newFormset);
 
+        // Update the visibility of the "Remove Ingredient" button
+        updateRemoveButtonVisibility();
+        
         // Update formset management form
-        updateFormsetManagementForm(formsetContainer, formsetContainerCount + 1);
+        updateFormsetManagementForm(formsetContainer, formsets.length);
+    });
+
+    // Remove form when the "Remove Ingredient" button is clicked
+    removeFormButton.addEventListener('click', function () {
+        var formsets = formsetContainer.getElementsByClassName('formset');
+        if (formsets.length > 1) {
+            var lastFormset = formsets[formsets.length - 1];
+            formsetContainer.removeChild(lastFormset);
+
+            // Update the visibility of the "Remove Ingredient" button
+            updateRemoveButtonVisibility();
+            
+            // Update formset management form
+            updateFormsetManagementForm(formsetContainer, formsets.length - 1);
+        }
     });
 });
 
 // Logic used to add dynamically more forms/lines to the allergens formset on frontend, to allow users
 // to add as much allergens to their recipe as they want when SUBMITTING recipes.
-
 document.addEventListener('DOMContentLoaded', function () {
     var formsetContainer = document.getElementById('allergens-formset-container');
 
@@ -51,7 +74,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Logic used to add dynamically more forms/lines to the cooking instructions formset on frontend, 
 // to allow users to add as much cooking instructions to their recipe as they want when SUBMITTING recipes.
-
 document.addEventListener('DOMContentLoaded', function () {
     var formsetContainer = document.getElementById('cooking-instructions-formset-container');
 
@@ -74,7 +96,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Logic used to add dynamically more forms/lines to the cooking tools formset on frontend, 
 // to allow users to add as much cooking tools to their recipe as they want when SUBMITTING recipes.
-
 document.addEventListener('DOMContentLoaded', function () {
     var formsetContainer = document.getElementById('recipe-tools-formset-container');
 
@@ -97,7 +118,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Logic used to add dynamically more forms/lines to the similar recipes formset on frontend, 
 // to allow users to add as much similar recipes to their recipe as they want when SUBMITTING recipes.
-
 document.addEventListener('DOMContentLoaded', function () {
     var formsetContainer = document.getElementById('recipe-similar-complementary-formset-container');
 
@@ -125,6 +145,20 @@ function updateFormInputNames(form, formCount) {
         input.setAttribute('name', name);
         input.value = '';
     });
+}
+
+function updateRemoveButtonVisibility() {
+    var formsetContainer = document.getElementById('ingredients-formset-container');
+    var removeFormButton = document.getElementById('remove-ingredient-form');
+
+    var formsets = formsetContainer.getElementsByClassName('formset');
+    console.log('Number of formsets:', formsets.length);
+
+    if (formsets.length > 2) {
+        removeFormButton.style.display = 'inline-block';
+    } else {
+        removeFormButton.style.display = 'none';
+    }
 }
 
 function updateFormsetManagementForm(formsetContainer, formCount) {
